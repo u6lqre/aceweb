d4cc16d07a3682e52b9bec1a13fe3c2c1902b857
u6lqre <ozwtn9@tutamail.com>
2025-11-25 11:53:46 +0100

    frontend: correct CustomFragmentLoader class

--- frontend/src/utils/CustomFragmentLoader.ts
+++ frontend/src/utils/CustomFragmentLoader.ts
@@ -1,24 +1,42 @@
 import Hls, {
   type FragmentLoaderContext,
   type HlsConfig,
+  type Loader,
   type LoaderCallbacks,
   type LoaderConfiguration,
-  type LoaderContext,
 } from "hls.js";
 import { rewriteEngineUrl } from "./rewriteEngineUrl";
 
-export class CustomFragmentLoader extends Hls.DefaultConfig.loader {
+export class CustomFragmentLoader implements Loader<FragmentLoaderContext> {
+  private loader: Loader<FragmentLoaderContext>;
+
   constructor(config: HlsConfig) {
-    super(config);
+    const DefaultLoader = Hls.DefaultConfig.loader;
+    this.loader = new DefaultLoader(config) as Loader<FragmentLoaderContext>;
+  }
+
+  get stats() {
+    return this.loader.stats;
+  }
+
+  get context() {
+    return this.loader.context;
+  }
+
+  destroy() {
+    this.loader.destroy();
+  }
+
+  abort() {
+    this.loader.abort();
   }
 
   load(
     context: FragmentLoaderContext,
     config: LoaderConfiguration,
-    callbacks: LoaderCallbacks<LoaderContext>
+    callbacks: LoaderCallbacks<FragmentLoaderContext>
   ) {
     context.url = rewriteEngineUrl(context.url);
-
-    super.load(context, config, callbacks);
+    this.loader.load(context, config, callbacks);
   }
 }
